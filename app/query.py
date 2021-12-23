2d5b6047e95e00a7c0eec7aecad1a948856faa51
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2021-12-23 00:44:51 +0000

    Implement dynamic whois functionality

--- app/query.py
+++ app/query.py
@@ -1,14 +1,23 @@
 from app import app
 from flask import request, render_template
+from .query_form import QueryForm
 import whois
 
 @app.route("/")
 
 def query():
-    search = request.args.get("search")
-    results = get_whois_data(search)
+    form = QueryForm(request.args, meta={'csrf': False})
 
-    return render_template("query.html", search = search, results = results)
+    search = request.args.get("search")
+
+    results = get_whois_data(search) if form.validate() else None
+
+    return render_template(
+        "query.html",
+        form = form,
+        search = search,
+        results = results
+    )
 
 def get_whois_data(search):
-    if search: return str(whois.whois(search))
+    return str(whois.whois(search))
---
+++ app/query_form.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, ValidationError
+import re
+
+class QueryForm(FlaskForm):
+    search = StringField('Enter domain', validators=[DataRequired()])
+
+    def validate_search(form, field):
+        if not re.match(r'[A-z0-9\-\.]+\.[A-z0-9\-\.]+', str(field.data)):
+            raise ValidationError("Invalid domain format")
